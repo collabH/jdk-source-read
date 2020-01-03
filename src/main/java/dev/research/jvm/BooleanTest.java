e5baf8f72a5701337d798e9ad6ea4194320cd2e9
shimin.huang <h1261109615@qq.com>
2020-01-04 00:28:37 +0800

    feat: 增加boolean字节码

--- src/main/java/dev/research/jvm/BooleanTest.java
+++ src/main/java/dev/research/jvm/BooleanTest.java
@@ -8,8 +8,8 @@ package dev.research.jvm;
  */
 public class BooleanTest {
     public static void main(String[] args) {
-        boolean flag=true;
-        if (flag) System.out.println("hello world1");
-        if (flag==true) System.out.println("hello world2");
+        boolean flag=true;//iload iconst_1 istore 1
+        if (flag) System.out.println("hello world1"); //iload 1 ifeql l3 iconst_1
+        if (flag==true) System.out.println("hello world2");//iload 1 iconst_1
     }
 }
